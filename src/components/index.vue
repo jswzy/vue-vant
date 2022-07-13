52d3ea627a6b736d628e8917f7d369b789ec0df5
jswzy <44564159+jswzy@users.noreply.github.com>
2022-07-13 17:32:46 +0800

    Create index.vue
    add index.vue

---
+++ src/components/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <van-button type="primary">主要按钮</van-button>
+    <h2>Essential Links</h2>
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
+<!-- <van-rate v-model="value"  /> -->
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Button,Swipe,SwipeItem,Rate} from 'vant';
+Vue.use(Button);
+Vue.use(Swipe);
+Vue.use(SwipeItem);
+Vue.use(Rate);
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      value: 2,
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.my-swipe .van-swipe-item {
+    color: #fff;
+    font-size: 20px;
+    line-height: 150px;
+    text-align: center;
+    background-color: #39a9ed;
+  }
+</style>
