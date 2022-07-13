d2aea63b03038958b3fe7ab6a026450a8d8d30a4
wangzhiy <wangzhiyuan@yonyou.com>
2022-07-13 16:18:45 +0800

    init,引入vanSwipe

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<!--
+ * @Author: wangzhiy
+ * @Date: 2022-06-13 21:00:55
+ * @LastEditors: wangzhiy
+ * @LastEditTime: 2022-06-13 21:25:29
+ * @Description:  
+-->
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
